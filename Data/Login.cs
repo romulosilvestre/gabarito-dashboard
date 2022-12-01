c70754d2d95ed060fc6e5f2e33f79ae51eea2f08
romulosilvestre <romulosilvestre@df.estudante.senai.br>
2022-12-01 20:32:32 -0300

    adicionando projeto Daniel

---
+++ Data/Login.cs
@@ -0,0 +1,7 @@
+using Newtonsoft.Json;
+public class Login{  
+   [JsonProperty("Usuario")] 
+   public string? Usuario { get; set; }
+   [JsonProperty("DataCriacao")]
+   public DateTime DataCriacao { get; set; } 
+}
