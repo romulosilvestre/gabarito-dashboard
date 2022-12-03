0f40c726e31ca5ae96b1fa4f72bed099d787d338
romulosilvestre <romulosilvestre@df.estudante.senai.br>
2022-12-03 13:43:11 -0300

    inicio do gabarito

---
+++ Data/Funcionario.cs
@@ -0,0 +1,5 @@
+using Newtonsoft.Json;
+
+public class Funcionario{
+    
+}
--- Data/Login.cs
+++ Data/Login.cs
@@ -1,7 +1,10 @@
 using Newtonsoft.Json;
-public class Login{  
+public class Login{ 
+    
    [JsonProperty("Usuario")] 
    public string? Usuario { get; set; }
-   [JsonProperty("DataCriacao")]
-   public DateTime DataCriacao { get; set; } 
+
+   [JsonProperty("Senha")] 
+   public string? Senha { get; set; }
+
 }
